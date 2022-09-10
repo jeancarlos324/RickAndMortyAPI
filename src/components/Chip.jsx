75f2fdee163c63505058c078681523b17e4e416f
jeanticona <jecar324@gmail.com>
2022-09-10 10:58:11 -0500

    testing

--- src/components/Chip.jsx
+++ src/components/Chip.jsx
@@ -7,7 +7,7 @@ const Chip = () => {
       className="bg-slate-600 w-fit text-white p-3 cursor-pointer rounded-md"
       whileHover={{ scale: 0.9 }}
       transition={{ type: "spring", stiffness: 400, damping: 10 }}
-    >RickAndMorty</motion.div>
+    >RickAndMortys</motion.div>
   );
 };
 
